48166e8080c806aae8cfbff17de55feed723b601
Elena Radiushkina <lenaradiushkina@gmail.com>
2018-07-07 21:54:26 -0500

    changed variable and move Contact

--- group.py
+++ group.py
@@ -3,28 +3,3 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
-
-
-class Contact:
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
-                 email, email2, email3, homepage, byear, ayear, address2, phone2, notes):
-        self.firstname = firstname
-        self.middlename = middlename
-        self.lastname = lastname
-        self.nickname = nickname
-        self.title = title
-        self.company = company
-        self.address = address
-        self.home = home
-        self.mobile = mobile
-        self.work = work
-        self.fax = fax
-        self.email = email
-        self.email2 = email2
-        self.email3 = email3
-        self.homepage = homepage
-        self.byear = byear
-        self.ayear = ayear
-        self.address2 = address2
-        self.phone2 = phone2
-        self.notes = notes
