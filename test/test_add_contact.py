c690ef08675cd78b90d408e5b35d3a63e15cbb00
Elena Radiushkina <lenaradiushkina@gmail.com>
2018-07-08 01:37:42 -0500

    add class ContactHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname='firstname', middlename='middlename', lastname='lastname',
+    app.contact.create(Contact(firstname='firstname', middlename='middlename', lastname='lastname',
                                         nickname='nickname', title='title', company='company', address='address',
                                         home='home', mobile='mobile', work='work', fax='fax', email='email',
                                         email2='email2', email3='email3', homepage='homepage', byear='byear',
@@ -23,7 +23,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
+    app.contact.create(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
                                         address='', home='', mobile='', work='', fax='', email='', email2='', email3='',
                                         homepage='', byear='', ayear='', address2='', phone2='', notes=''))
     app.session.logout()
