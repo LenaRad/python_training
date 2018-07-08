e31cd0917bf574e1f9558fe97533de2cdc4c40bd
Elena Radiushkina <lenaradiushkina@gmail.com>
2018-07-08 01:08:47 -0500

    add class SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,18 +12,18 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname='firstname', middlename='middlename', lastname='lastname',
                                         nickname='nickname', title='title', company='company', address='address',
                                         home='home', mobile='mobile', work='work', fax='fax', email='email',
                                         email2='email2', email3='email3', homepage='homepage', byear='byear',
                                         ayear='ayear', address2='address2', phone2='phone2', notes='notes'))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname='', middlename='', lastname='', nickname='', title='', company='',
                                         address='', home='', mobile='', work='', fax='', email='', email2='', email3='',
                                         homepage='', byear='', ayear='', address2='', phone2='', notes=''))
-    app.logout()
+    app.session.logout()
