25c7bc6955ff23a7db22eb41aac855f9dbdc8369
Elena Radiushkina <lenaradiushkina@gmail.com>
2018-08-07 16:01:52 +0300

    fixed edit group and contact

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,12 +2,12 @@
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
+def test_edit_firstname_first_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first(Contact(firstname='new firstname', middlename='new middlename', lastname='new lastname',
-                                   nickname='new nickname', title='new title', company='new company',
-                                   address=' new address', home=' new home', mobile='new mobile', work='new work',
-                                   fax='new fax', email='new email', email2='new email2', email3='new email3',
-                                   homepage='new homepage', byear='2018', ayear='2018',
-                                   address2='new address2', phone2='new phone2', notes='new notes'))
+    app.contact.edit_first(Contact(firstname='new firstname'))
+    app.session.logout()
+
+def test_edit_company_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first(Contact(company='new company'))
     app.session.logout()
