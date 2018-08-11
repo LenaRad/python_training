736215c89f122ea3a2311c62a07b4f6e86150d05
Elena Radiushkina <lenaradiushkina@gmail.com>
2018-08-11 10:31:53 +0300

    add dynamic test generation

---
+++ data/contacts.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname='firstname', middlename='middlename', lastname='lastname', nickname='nickname', title='title',
+            company='company', address='address', home='home', mobile='mobile', work='work', fax='fax', email='email',
+            email2='email2', email3='email3', homepage='homepage', byear='byear', ayear='ayear', address2='address2',
+            phone2='phone2', notes='notes')
+
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.add_contact import testdata
 
-@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contact = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contact = app.contact.get_contact_list()
